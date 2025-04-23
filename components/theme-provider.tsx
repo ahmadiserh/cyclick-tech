0fb6a96ba79cd63d5b15f2721ecd2b7f6c634416
Ahmad Isah <ahmadisah6293@gmail.com>
2025-04-23 12:49:07 +0100

    working

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,6 +2,6 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  return <NextThemesProvider attribute="class" defaultTheme="dark">{children}</NextThemesProvider>
 }
